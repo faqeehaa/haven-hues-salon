3c9ad48c8be1b0a6184fc5e5d9e5d0d8d87a38f7
faqeehaa <faqeeha.muqadas@gmail.com>
2024-05-11 17:17:21 +0500

    Beauty salon

---
+++ dom_js.js
@@ -0,0 +1,5 @@
+const image = document.getElementById('unicorn');
+const heading = document.getElementById('mainheading');
+const doneTodos = document.querySelectorAll('.done');
+ const checkbox = document.querySelector('input[type="checkbox"]');
+ //template literal rgb colors
